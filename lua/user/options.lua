4a6358261c1ad6ce6104a4c2787d38bfe9b0d785
Blackbeard <67589385+blackbeard173@users.noreply.github.com>
2023-01-22 21:38:13 +0530

    feat: options

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,1 +1,28 @@
-vim.opt.background = "dark"
+local opt = vim.opt
+
+opt.termguicolors = true
+opt.background = "dark"
+
+opt.number = false
+opt.relativenumber = false
+
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+
+opt.smartindent = true
+
+opt.wrap = true
+
+opt.swapfile = false
+opt.backup = false
+
+opt.hlsearch = false
+opt.incsearch = true
+
+opt.scrolloff = 8
+opt.signcolumn = "yes"
+opt.isfname:append("@-@")
+
+opt.updatetime = 50
