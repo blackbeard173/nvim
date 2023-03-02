9a603dd032213d1a449ee12eedc6cc03813eaf1d
Blackbeard <67589385+blackbeard173@users.noreply.github.com>
2023-03-02 12:34:01 +0530

    feat: add keymap

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,6 +1,8 @@
 local map = vim.keymap.set
 
-map('n', '<leader>e', '<cmd>Lexplore<cr>', { desc = 'File Explorer' })
-map('n', '<leader>l', '<cmd>Lazy<cr>', { desc = 'Lazy' })
+map('n', '<leader>e', '<cmd>:Lexplore<cr>', { desc = 'File Explorer' })
+map("n", "<leader>l", "<cmd>:Lazy<cr>", { desc = "Lazy" })
 map('n', '<leader>,', '<cmd>e $MYVIMRC<cr>', { desc = 'Config' })
-map("n", "<leader>f", vim.lsp.buf.format, { desc = 'Format' })
+map("n", "<leader>ff", vim.lsp.buf.format, { desc = 'Format' })
+
+map("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit all" })
