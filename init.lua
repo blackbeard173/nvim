4ebe2207d07db440d18a3d295324867a057c8316
Blackbeard <67589385+blackbeard173@users.noreply.github.com>
2023-04-05 11:44:08 +0530

    telescope

--- init.lua
+++ init.lua
@@ -1,23 +1,33 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system(
-        {
-            "git",
-            "clone",
-            "--filter=blob:none",
-            "https://github.com/folke/lazy.nvim.git",
-            "--branch=stable",
-            lazypath
-        }
-    )
+	vim.fn.system(
+	{
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath
+	}
+	)
 end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
 	{"nyoom-engineering/oxocarbon.nvim"},
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.1",
+		cmd = "Telescope",
+		keys = { 
+			{ "<leader>f","<cmd>Telescope find_files<cr>", desc = "Find Files" }
+		}
+
+	},
+	{"nvim-lua/plenary.nvim"}
 }
 local opts = {
-    ui = {
+	ui = {
 		border = "rounded",
 	},
 	checker = {
@@ -31,3 +41,5 @@ require("lazy").setup(plugins, opts)
 
 vim.opt.background = "dark"
 vim.cmd.colorscheme "oxocarbon"
+
+vim.keymap.set("n","<leader>l", "<cmd>Lazy<cr>",{desc = "Lazy"})
