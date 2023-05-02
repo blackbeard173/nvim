2e0d6e0ccef4c94ae508830cfb3d2af3e893ddf9
Blackbeard <67589385+blackbeard173@users.noreply.github.com>
2023-05-02 10:58:48 +0530

    feat: colorschemes- tokyonight,oxocarbon

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 vim.g.mapleader = " "
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -6,9 +7,26 @@ if not vim.loop.fs_stat(lazypath) then
     "clone",
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
+    "--branch=stable",
     lazypath,
   })
 end
 vim.opt.rtp:prepend(lazypath)
+
+local plugins={
+  {'folke/tokyonight.nvim'},
+  {'nyoom-engineering/oxocarbon.nvim'},
+
+}
+local opts={
+  ui={border="rounded",},
+  checker={enabled=true,},
+}
 require("lazy").setup(plugins, opts)
+
+vim.cmd.colorscheme("tokyonight-night")
+
+-- keymaps
+vim.keymap.set("n","<leader>l","<cmd>Lazy<cr>")
+vim.keymap.set("n","<leader>,","<cmd>e $MYVIMRC<cr>")
+vim.keymap.set("n","<leader>e",vim.cmd.Lexplore)
