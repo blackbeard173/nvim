164014451631c90fc206cc6146d582c74243212c
Blackbeard <67589385+blackbeard173@users.noreply.github.com>
2023-05-20 22:57:27 +0530

    style: formatting 2->4 spaces

--- init.lua
+++ init.lua
@@ -3,78 +3,78 @@ vim.g.maplocalleader = ' '
 
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable',
-    lazypath,
-  })
+    vim.fn.system({
+        'git',
+        'clone',
+        '--filter=blob:none',
+        'https://github.com/folke/lazy.nvim.git',
+        '--branch=stable',
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
-  { 'folke/tokyonight.nvim' },
-  { 'nyoom-engineering/oxocarbon.nvim' },
+    { 'folke/tokyonight.nvim' },
+    { 'nyoom-engineering/oxocarbon.nvim' },
 
-  { 'nvim-telescope/telescope.nvim', dependencies = { 'nvim-lua/plenary.nvim' }, cmd = 'Telescope' },
-  {
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
-    opts = { auto_install = true, highlight = { enable = true }, indent = { enable = true } },
-    config = function(_, opts)
-      require('nvim-treesitter.configs').setup(opts)
-    end,
-  },
-
-  {
-    'echasnovski/mini.statusline',
-    config = function(_, opts)
-      require('mini.statusline').setup(opts)
-    end,
-  },
-  {
-    'echasnovski/mini.comment',
-    keys = { 'gc', 'gcc' },
-    config = function(_, opts)
-      require('mini.comment').setup(opts)
-    end,
-  },
-  {
-    'echasnovski/mini.indentscope',
-    opts = { symbol = '│' },
-    config = function(_, opts)
-      require('mini.indentscope').setup(opts)
-    end,
-  },
-  {
-    'echasnovski/mini.pairs',
-    config = function(_, opts)
-      require('mini.pairs').setup(opts)
-    end,
-  },
-  {
-    'echasnovski/mini.basics',
-    opts = {
-      basic = true,
-      extra_ui = true,
-      win_borders = 'rounded',
+    { 'nvim-telescope/telescope.nvim', dependencies = { 'nvim-lua/plenary.nvim' }, cmd = 'Telescope' },
+    {
+        'nvim-treesitter/nvim-treesitter',
+        build = ':TSUpdate',
+        opts = { auto_install = true, highlight = { enable = true }, indent = { enable = true } },
+        config = function(_, opts)
+            require('nvim-treesitter.configs').setup(opts)
+        end,
     },
-    config = function(_, opts)
-      require('mini.basics').setup(opts)
-    end,
-  },
+
+    {
+        'echasnovski/mini.statusline',
+        config = function(_, opts)
+            require('mini.statusline').setup(opts)
+        end,
+    },
+    {
+        'echasnovski/mini.comment',
+        keys = { 'gc', 'gcc' },
+        config = function(_, opts)
+            require('mini.comment').setup(opts)
+        end,
+    },
+    {
+        'echasnovski/mini.indentscope',
+        opts = { symbol = '│' },
+        config = function(_, opts)
+            require('mini.indentscope').setup(opts)
+        end,
+    },
+    {
+        'echasnovski/mini.pairs',
+        config = function(_, opts)
+            require('mini.pairs').setup(opts)
+        end,
+    },
+    {
+        'echasnovski/mini.basics',
+        opts = {
+            basic = true,
+            extra_ui = true,
+            win_borders = 'rounded',
+        },
+        config = function(_, opts)
+            require('mini.basics').setup(opts)
+        end,
+    },
 }
 
 local opts = {
-  ui = { border = 'rounded' },
-  checker = { enabled = true },
+    ui = { border = 'rounded' },
+    checker = { enabled = true },
 }
 
 require('lazy').setup(plugins, opts)
 
-vim.cmd.colorscheme('tokyonight-night')
+vim.cmd.colorscheme('oxocarbon')
 
 -- keymaps
 vim.keymap.set('n', '<leader>l', '<cmd>Lazy<cr>')
