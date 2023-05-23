6b17c0d77146177a1497b94a8a1767c0d7752716
Blackbeard <67589385+blackbeard173@users.noreply.github.com>
2023-05-23 11:49:04 +0530

    refactor: lazy.nvim

--- init.lua
+++ init.lua
@@ -1,25 +1,7 @@
+require('configs.lazy')
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		'git',
-		'clone',
-		'--filter=blob:none',
-		'https://github.com/folke/lazy.nvim.git',
-		'--branch=stable',
-		lazypath,
-	})
-end
-vim.opt.rtp:prepend(lazypath)
-
-local opts = {
-	ui = { border = 'rounded' },
-	checker = { enabled = false },
-}
-
-require('lazy').setup('plugins', opts)
 
 vim.cmd.colorscheme('tokyonight-night')
 
